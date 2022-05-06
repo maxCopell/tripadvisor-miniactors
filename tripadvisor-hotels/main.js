cea2afbe42c4fcce6e5912c73623ad041ff067b9
Zuzana Pelechová <zpelechova@gmail.com>
2022-05-06 11:43:03 +0200

    changed source actor to tripadvisor-source

--- tripadvisor-hotels/main.js
+++ tripadvisor-hotels/main.js
@@ -1,15 +1,15 @@
-import Apify from 'apify';
+import Apify from 'apify'
 
 Apify.main(async () => {
-    const input = await Apify.getInput();
-    await Apify.metamorph('maxcopell/tripadvisor', {
-        ...input,
-      includeAttractions: false,
-      includeRestaurants: false,
-      includeHotels: true,
-      includeTags: true,
-        proxy: {
-            "useApifyProxy": true,
-        },
-    });
-});
+  const input = await Apify.getInput()
+  await Apify.metamorph('maxcopell/tripadvisor-source', {
+    ...input,
+    includeAttractions: false,
+    includeRestaurants: false,
+    includeHotels: true,
+    includeTags: true,
+    proxy: {
+      useApifyProxy: true
+    }
+  })
+})
--- tripadvisor-paid/main.js
+++ tripadvisor-paid/main.js
@@ -1,15 +1,15 @@
-import Apify from 'apify';
+import Apify from 'apify'
 
 Apify.main(async () => {
-    const input = await Apify.getInput();
-    await Apify.metamorph('maxcopell/tripadvisor', {
-        ...input,
-      includeAttractions: false,
-      includeRestaurants: true,
-      includeHotels: false,
-      includeTags: true,
-        proxy: {
-            "useApifyProxy": true,
-        },
-    });
-});
+  const input = await Apify.getInput()
+  await Apify.metamorph('maxcopell/tripadvisor-source', {
+    ...input,
+    includeAttractions: false,
+    includeRestaurants: true,
+    includeHotels: false,
+    includeTags: true,
+    proxy: {
+      useApifyProxy: true
+    }
+  })
+})
