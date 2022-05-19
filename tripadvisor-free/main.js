7fc63bb717a5a86d462ba3cdf2c1cdcefb11f6bd
Zuzana Pelechová <zpelechova@gmail.com>
2022-05-19 08:13:23 +0200

    fixed only restaurants in free and paid :/

--- tripadvisor-free/main.js
+++ tripadvisor-free/main.js
@@ -4,12 +4,5 @@ Apify.main(async () => {
   const input = await Apify.getInput()
   await Apify.metamorph('maxcopell/tripadvisor-source', {
     ...input,
-    includeAttractions: false,
-    includeRestaurants: true,
-    includeHotels: false,
-    includeTags: true,
-    proxy: {
-      useApifyProxy: true
-    }
   })
 })
--- tripadvisor-paid/main.js
+++ tripadvisor-paid/main.js
@@ -12,13 +12,6 @@ Apify.main(async () => {
   
   await Apify.metamorph('maxcopell/tripadvisor-source', {
     ...input,
-    includeAttractions: false,
-    includeRestaurants: true,
-    includeHotels: false,
-    includeTags: true,
-    proxy: {
-      useApifyProxy: true
-    }
   }, {
     build: await isBetaVersion() ? 'beta' : 'latest',
   })
